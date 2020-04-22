3d05912b63ea53d95e65d5520053e3535ed2b73f
startergate <startergate36@gmail.com>
2020-04-22 12:50:29 +0900

    misc: test code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import * as readline from 'readline';
 
 import axios from 'axios';
+import subscribe from "./dpnd";
 
 const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
 rl.question('STEAM ID> ', (id: string) => {
     rl.question('APP NAME> ', (appName: string) => {
-
+        subscribe(id, 'random', parseInt(appName));
     });
 });
