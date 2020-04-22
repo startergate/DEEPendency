d9db3acf0831eac4d5248a9c81fc4cc53c19d03a
startergate <startergate36@gmail.com>
2020-04-22 12:50:05 +0900

    add: subscribing function

--- src/dpnd.ts
+++ src/dpnd.ts
@@ -3,3 +3,13 @@ import * as cheerio from 'cheerio';
 
 const getUrl = (userid: string, appid: number) =>
     `https://steamcommunity.com/id/${userid}/myworkshopfiles/?appid=${appid}&browsefilter=mysubscriptions`;
+
+const subscriber = async (appid: number, assetid: number, session: string) => {
+    await axios.post('https://steamcommunity.com/sharedfiles/subscribe', {
+        id: assetid,
+        appid: appid,
+        sessionid: session
+    }, {
+        headers: {}
+    });
+};
