e7532b653426451b6171c74f92dee8cc27772148
startergate <startergate36@gmail.com>
2020-06-12 13:31:59 +0900

    misc: close rl interface after job done

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import subscribe from "./dpnd";
 
 const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
-rl.question('STEAM ID> ', (id: string) => {
-    rl.question('APP NAME> ', async (appName: string) => {
+rl.question('STEAM ID> ', async (id: string) => {
+    await rl.question('APP NAME> ', async (appName: string) => {
         await subscribe(id, 'random', parseInt(appName));
+        rl.close();
     });
 });
